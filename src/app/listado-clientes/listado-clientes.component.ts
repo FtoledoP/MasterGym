732e552c1b5643d910e238e3c92425516d27cac5
franco <fr.toledop@duocuc.cl>
2023-07-30 17:26:21 -0400

    avance

--- src/app/listado-clientes/listado-clientes.component.ts
+++ src/app/listado-clientes/listado-clientes.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-listado-clientes',
   templateUrl: './listado-clientes.component.html',
   styleUrls: ['./listado-clientes.component.scss']
 })
 export class ListadoClientesComponent {
+  clientes: any[] = new Array<any>()
 
+  
 }
