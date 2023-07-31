6955d94ec4ba96e6244e4eddd41e46ecc43dcaf1
franco <fr.toledop@duocuc.cl>
2023-07-31 03:38:25 -0400

    update

--- src/app/agregar-cliente/agregar-cliente.component.ts
+++ src/app/agregar-cliente/agregar-cliente.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Storage } from '@angular/fire/storage';
+import { ref, uploadBytesResumable, UploadTaskSnapshot, getDownloadURL } from 'firebase/storage';
+import { Firestore } from '@angular/fire/firestore';
+import { addDoc, collection } from 'firebase/firestore';
 
 @Component({
   selector: 'app-agregar-cliente',
@@ -9,34 +12,61 @@ import { Storage } from '@angular/fire/storage';
 })
 export class AgregarClienteComponent {
   formularioCliente: FormGroup = {} as FormGroup;
+  porcentajeSubida: number = 0;
+  urlImagen:string =""
 
-  constructor(private fb: FormBuilder, private storage: Storage){
+  constructor(private fb: FormBuilder, private storage: Storage, private db: Firestore) {}
 
-  }
-
-  ngOnInit(){
+  ngOnInit() {
     this.formularioCliente = this.fb.group({
       nombre: ['', Validators.required],
       apellido: ['', Validators.required],
-      correo: ['', Validators.compose([
-        Validators.required, Validators.email
-      ])],
+      correo: ['', Validators.compose([Validators.required, Validators.email])],
       cedula: [''],
       fechaNacimiento: ['', Validators.required],
       telefono: [''],
       imgUrl: ['', Validators.required]
+    });
+  }
+
+  agregar() {
+    this.formularioCliente.value.imgUrl = this.urlImagen
+    this.formularioCliente.value.fechaNacimiento = new Date(this.formularioCliente.value.fechaNacimiento)
+    console.log(this.formularioCliente.value);
+    addDoc(collection(this.db, 'clientes'), this.formularioCliente.value)
+    .then((resultado) =>{
+      console.log("Registrado");
     })
   }
 
-  agregar(){
-    console.log(this.formularioCliente.value);
+  subirImagen(evento: any) {
+    if(evento.target.files.length > 0){
+      let nombre = new Date().getTime().toString()
+      let archivo = evento.target.files[0];
+      let extension = archivo.name.toString().substring(archivo.name.toString().lastIndexOf('.'))
+
+      const storageRef = ref(this.storage, `clientes/${nombre}${extension}`);
+      const tareaSubida = uploadBytesResumable(storageRef, archivo);
+
+      tareaSubida.on(
+        'state_changed',
+        (snapshot: UploadTaskSnapshot) => {
+          // Calcula el porcentaje de subida
+          this.porcentajeSubida = parseInt(((snapshot.bytesTransferred / snapshot.totalBytes * 100).toString()));
+        },
+        (error: any) => {
+          console.log('Error al subir el archivo:', error);
+        },
+        () => {
+          console.log('Archivo subido exitosamente.');
+          getDownloadURL(storageRef)
+          .then((respuesta) => {
+            this.urlImagen = respuesta
+          })
+          .catch((error) => console.log(error))
+
+      }
+    );
+    }
   }
-
-  subirImagen(evento:any){
-    
-    let archivo = evento.target.files[0]
-
-    console.log(archivo);
-  }
-
 }
